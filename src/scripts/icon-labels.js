dd7204b6c15733c0940265281c502399524e75a8
shoeiedibistl <shoeiedibistl2@gmail.com>
2024-03-05 11:19:20 +0300

    партнерам

--- src/scripts/icon-labels.js
+++
@@ -1,7 +0,0 @@
-const icons = $("[data-card-icon]");
-
-$(icons).each((index, icon) => {
-  $(icon).on("click", function (e) {
-    e.preventDefault();
-  });
-});
--- src/scripts/scroll-top.js
+++
@@ -1,3 +0,0 @@
-const upBtn = $('[data-btn="data-up"]')
-
-upBtn.click(() => $('html, body').animate({ scrollTop: 0 }, 500))
